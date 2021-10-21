5b24e0a937c1a6e7fbadb05f985c9d30efe18e86
Vlad Polonsky <vladpolonsky91@gmail.com>
2021-10-21 15:26:17 +0300

    added the  the func that change color of  to the title

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const title = document.querySelectorAll("h1")
-const words = document.querySelectorAll(".words")
-const colors = document.querySelectorAll(".colors")
+const title = document.querySelector("h1")
+const words = document.querySelector(".words")
+const colors = document.querySelector(".colors")
 
 words.addEventListener('click',function(){
 title.textContent += "  Blah"
 })
+words.addEventListener('click',function(){
+    title.style.color = title.style.color == "red" ? "black" : "red"
+})
+    
